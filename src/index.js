7e6ae566b2c71df8e2ce315b8f1ed7ef34c2a25a
Zhang Kai Yu <yeannylam@gmail.com>
2016-02-18 12:07:10 +0800

    Easy require

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import createRootPage from './reducers/createRootPage';
+import renderRootPage from './components/renderRootPage';
+
+import Page from './components/Page';
+
+export {
+  createRootPage,
+  renderRootPage,
+  Page
+};
