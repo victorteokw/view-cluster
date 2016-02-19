81174c806160610c62a683a0ccaa86e5f8d85339
Zhang Kai Yu <yeannylam@gmail.com>
2016-02-19 20:25:08 +0800

    Update render root page to accept children

--- src/components/renderRootPage.jsx
+++ src/components/renderRootPage.jsx
@@ -3,9 +3,11 @@ import * as defaultPages from './pages';
 
 import merge from 'lodash/merge';
 
-export default function renderRootPage(state, pages, dispatch) {
+export default function renderRootPage(state, pages, dispatch, children = null) {
   pages = merge({}, pages, defaultPages);
   let Page = pages[state.page];
   let props = state.props;
-  return <Page path={[]} {...props} pages={pages} dispatch={dispatch} root={true} />
+  return <Page path={[]} {...props} pages={pages} dispatch={dispatch} root={true}>
+    {children}
+  </Page>
 }
